2607c05800f1aa993ef39b29746e312332f0ae47
marekokrent <marek.okrent@gmail.com>
2020-04-25 22:07:35 +0200

    remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 'use strict';
 
 /* remove class 'active' from all article links  */
-const acitiveLinks = document.querySelectorAll('.ticle a .active');
+const acitiveLinks = document.querySelectorAll('.titles a .active');
 for (let activeLink of acitiveLinks){
   activeLink.classList.remove('active');
 }
+
+const acitiveArticles = document.querySelectorAll('.posts article .active');
+for (let activeArticle of acitiveArticles){
+  activeArticle.classList.remove('active');
+}
+
 /* add class 'active' to the clicked link */
+
+
 
 /* remove class 'active' from all articles */
 
@@ -18,6 +26,8 @@ for (let activeLink of acitiveLinks){
 function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log(event);
+  const clickedElement = this;
+  console.log('clickedElement:', clickedElement);
 }
 
 const links = document.querySelectorAll('.titles a');
